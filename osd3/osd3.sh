22673f32b834ea0ea2586ec7e12b310e74c8cdc1
Aliaksandr Dzeshchanka <llazy@tut.by>
2021-01-15 22:46:06 +0300

    Update osd3.sh

--- osd3/osd3.sh
+++ osd3/osd3.sh
@@ -180,7 +180,7 @@ sleep 3m
 sudo -u $user "cd /home/$user/openshift-ansible && ansible-playbook -i hosts.ini playbooks/prerequisites.yml" > /home/$user/ansible-deploy.log
 echo "ansible (no) prereq done" >> /root/status.log
 
-sudo -u $user "cd /home/$user/openshift-ansible && ansible-playbook -i hosts.ini playbooks/deploy_cluster.yml >> /home/$user/ansible-deploy.log
+sudo -u $user "cd /home/$user/openshift-ansible && ansible-playbook -i hosts.ini playbooks/deploy_cluster.yml" >> /home/$user/ansible-deploy.log
 echo "ansible (no) deploy done" >> /root/status.log
 
 echo "calling home" >> /root/status.log
